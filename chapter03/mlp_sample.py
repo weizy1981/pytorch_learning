db4ced603ec9e3cfa577b68e9ed48906fc2fabb0
kevin <kevin@yxtechs.cn>
2017-11-27 16:54:01 +0800

    chapter03

--- chapter03/mlp_sample.py
+++ chapter03/mlp_sample.py
@@ -2,12 +2,13 @@ from sklearn.datasets import load_iris
 from torch import nn
 from torch.autograd import Variable
 from torch import optim
+from torch.utils.data import DataLoader, TensorDataset
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import torch
 
 epochs = 150
-batch_size = 5
+batch_size = 150
 seed = 7
 lr = 0.01
 momentum = 0.9
@@ -27,6 +28,18 @@ class LogisticLinear(nn.Module):
         out = self.linear(x)
         out = self.softmax(out)
         return out
+def createTensorDataset(x, y):
+    # convert data to Tensors
+    x = torch.FloatTensor(x)
+
+    y = np.reshape(y, (y.shape[0], 1))
+    # one hot encodering
+    enc = OneHotEncoder(sparse=False, n_values=3)
+    y = enc.fit_transform(y).astype('float32')
+    y = torch.from_numpy(y)
+
+    dataset = TensorDataset(data_tensor=x, target_tensor=y)
+    return dataset
 
 if __name__ == '__main__':
 
@@ -44,37 +57,33 @@ if __name__ == '__main__':
     criterion = nn.BCELoss()
     optimizer = optim.SGD(params=model.parameters(), lr=lr, momentum=momentum)
 
-    # convert data to Tensors
-    x = torch.FloatTensor(x)
-    x = Variable(x)
-
-    y = np.reshape(y, (y.shape[0], 1))
-    # one hot encodering
-    enc = OneHotEncoder(sparse=False, n_values=3)
-    y = enc.fit_transform(y).astype('float32')
-    y = torch.from_numpy(y)
-    print(type(y))
-    y = Variable(y)
-
+    dataset = createTensorDataset(x, y)
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size)
     # training the model
     for epoch in range(epochs):
-        # forward pass
-        output = model(x)
+        for (data, label) in dataloader:
+            # convert to variable
+            data = Variable(data)
+            label = Variable(label)
+            # forward pass
+            output = model(data)
 
-        # calculate the loss
-        loss = criterion(output, y)
-        loss_data = loss.data[0]
+            # calculate the loss
+            loss = criterion(output, label)
+            loss_data = loss.data[0]
 
-        # reset gradients
-        optimizer.zero_grad()
-        # backward pass
-        loss.backward()
-        optimizer.step()
+            # reset gradients
+            optimizer.zero_grad()
+            # backward pass
+            loss.backward()
+            optimizer.step()
 
-        # print loss
-        print('{}/{} loss: {:.6f}'.format(epoch, epochs, loss_data))
+            # print loss
+            print('{}/{} loss: {:.6f}'.format(epoch, epochs, loss_data))
 
     model.eval()
+    x = torch.FloatTensor(x)
+    x = Variable(x)
     outputs = model(x)
     outputs = outputs.data.numpy()
     testacc = 0
